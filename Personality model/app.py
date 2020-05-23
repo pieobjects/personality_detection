b9c603059be03fcce6c4e153849f627099c5a781
ShivamVishwakarma <sv6375261073@gmail.com>
2020-05-24 00:26:34 +0530

    Update app.py

--- Personality model/app.py
+++ Personality model/app.py
@@ -2,14 +2,16 @@ from flask  import Flask,request,jsonify
 from model import Model
 import tensorflow as tf
 import keras
+from update_personality import update_prediction
 #import tensorflow_core.keras
 from prediction import Predictor_cat5,mbti_models
 app=Flask(__name__)
 Predictor_cat5_obj,mbti_models_obj=Predictor_cat5(),mbti_models()
 model=Model()
+probablity={}
 @app.route('/',methods=['POST'])
 def test():
-    probablity={}
+    global probablity
     if request.method=='POST':
         json_data=request.json
         text=json_data['text']
@@ -18,7 +20,14 @@ def test():
         probablity.update(cat5_probablity)
         probablity.update(mbti_probablity)
         return jsonify({'probablities':str(probablity)})
-        
+
+@app.route('/review',methods=['POST'])
+def update_personality_trades():
+    if request.method=='POST':
+        json_data=request.json
+        review=json_data['review']
+        probablity=update_prediction(probablity,review)
+        return jsonify({'probablities':str(probablity)})        
 if __name__=='__main__':
     app.run(debug=True,port=9090)
         
